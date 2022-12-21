5e8ffe5b62caad9e2717159f1cab8c0411f4c7cf
martbarc <martbarc@gmail.com>
2022-12-20 19:50:21 -0500

    main scene fixes

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,30 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UltimateClean;
 
 public class GameController : MonoBehaviour
 {
     [SerializeField] public CameraController camera_main;
 
+    [SerializeField] public Team team;
+
+
+    //UI
+    [SerializeField] public Popup popup_shop;
+    public bool pshopShow = false;
+
     public int season_num;
 
     private void Awake()
     {
-
         season_num = 1;
     }
 
-    // Start is called before the first frame update
     void Start()
     {
+        //ShowShop();
+    }
+
+    public void ShowShop()
+    {
+        if (pshopShow == false)
+        {
+            popup_shop.Open();
+            pshopShow = true;
+        }
+        else
+        {
+            popup_shop.Close();
+            pshopShow = false;
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    // // Start is called before the first frame update
+    // void Start()
+    // {
         
-    }
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
 
     
 }
